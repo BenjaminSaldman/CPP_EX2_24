6ebbe85376403ddd789893f3029bb854894dec66
BenjaminSaldman <93070344+BenjaminSaldman@users.noreply.github.com>
2024-04-30 08:15:18 +0300

    Update Demo.cpp

--- Demo.cpp
+++ Demo.cpp
@@ -22,7 +22,7 @@ int main()
         {0, 1, 0}};
     g1.loadGraph(graph); // Load the graph to the object.
 
-    g1.printGraph(); // Should print the matrix of the graph: [0, 1, 0], [1, 0, 1], [0, 1, 0]
+    cout<<g1; // Should print the matrix of the graph: [0, 1, 0], [1, 0, 1], [0, 1, 0]
 
     // 3x3 matrix that represents a weighted connected graph.
     vector<vector<int>> weightedGraph = {
@@ -33,14 +33,14 @@ int main()
     g2.loadGraph(weightedGraph); // Load the graph to the object.
 
     ariel::Graph g3 = g1 + g2; // Add the two graphs together.
-    g3.printGraph();           // Should print the matrix of the graph: [0, 2, 1], [2, 0, 3], [1, 3, 0]
+    cout<<g3;           // Should print the matrix of the graph: [0, 2, 1], [2, 0, 3], [1, 3, 0]
 
     g1 *= -2;        // Multiply the graph by -2.
-    g1.printGraph(); // Should print the matrix of the graph: [0, -2, 0], [-2, 0, -2], [0, -2, 0]
+    cout<<g1; // Should print the matrix of the graph: [0, -2, 0], [-2, 0, -2], [0, -2, 0]
 
     g1 /= -2;
     ariel::Graph g4 = g1 * g2; // Multiply the two graphs together.
-    g4.printGraph();           // Should print the multiplication of the matrices of g1 and g2: [0, 0, 2], [1, 0, 1], [1, 0, 0]
+    cout<<g4;           // Should print the multiplication of the matrices of g1 and g2: [0, 0, 2], [1, 0, 1], [1, 0, 0]
 
     // 5x5 matrix that represents a connected graph.
     vector<vector<int>> graph2 = {
